ec74b781ec10a1a23eaecaa8408837aa3cd5bc11
Sri Ratmurti <85623746+sriratmurti@users.noreply.github.com>
2021-10-18 10:04:10 +0700

    Initial Commit

---
+++ MacroChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MacroChallenge
+//
+//  Created by Sri Endah Ramurti on 18/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
